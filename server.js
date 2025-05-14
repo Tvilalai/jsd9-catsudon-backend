d303a98585e4a93ab3bdcf8a95c2e95c580379c5
Thanawat Panpinij <thanawatpanpinij@gmail.com>
2025-05-14 21:56:03 +0700

    feat(security): add security layer using rateLimiter helmet

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import { connectMongo } from './config/db.js';
-import apiRouter from './routes/apiRouter.js';
-import errorHandler from './middlewares/errorHandler.js';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import dotenv from "dotenv";
+import express from "express";
+import { connectMongo } from "./config/db.js";
+import apiRouter from "./routes/apiRouter.js";
+import errorHandler from "./middlewares/errorHandler.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import limiter from "./middlewares/rateLimiter.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const corsOptions = {
-    origin: ['http://localhost:5173', 'https://jsd9-catsudon-frontend.vercel.app'],
-    credentials: true,
-};
- 
-app.use(cors(corsOptions));
-app.use(cookieParser()); 
-app.use(express.json());
+app.set("trust proxy", 1);
 
-app.use('/calnoy-api/v1', apiRouter);
+app.use(helmet());
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://jsd9-catsudon-frontend.vercel.app",
+  ],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(limiter);
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/calnoy-api/v1", apiRouter);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
